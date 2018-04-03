337b9e194d1ba4b1abe444ec65448c40d750c2cf
Eric Ghildyal <ericghildyal@gmail.com>
2018-04-02 21:14:16 -0400

    Added code to manifest

--- app/src/main/java/com/example/der62/battlestocks/Login.java
+++ app/src/main/java/com/example/der62/battlestocks/Login.java
@@ -16,8 +16,14 @@ import android.content.Intent;
         import com.google.firebase.auth.AuthResult;
         import com.google.firebase.auth.FirebaseAuth;
         import com.google.firebase.auth.FirebaseUser;
-        import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
         import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Login extends AppCompatActivity {
 
@@ -28,6 +34,8 @@ public class Login extends AppCompatActivity {
     private EditText mPasswordField;
     protected FirebaseUser user = null;
     FirebaseDatabase database;
+    DatabaseReference reference;
+    ArrayList<HashMap> availableStocks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +47,71 @@ public class Login extends AppCompatActivity {
         mEmailField = findViewById(R.id.email);
         mPasswordField = findViewById(R.id.password);
         database = FirebaseDatabase.getInstance();
+        //connect to DB
+        reference = database.getReference();
+
+        // Get list of stocks
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                availableStocks = (ArrayList<HashMap>) dataSnapshot.child("AvailableStocks").getValue();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        // Get intent
+        Intent intent = getIntent();
+        String action = intent.getAction();
+
+        // Handle each intent correctly
+        if("edu.pitt.cs1699.team9.NEW_STOCK".equals(action)) {
+            newStock(intent);
+        }else if("edu.pitt.cs1699.team9.PRICE_CHANGE".equals(action)){
+            priceChange(intent);
+        }else if("edu.pitt.cs1699.team9.OFF_MARKET".equals(action)){
+            offMarket(intent);
+        }else if("edu.pitt.cs1699.team9.CRASH".equals(action)){
+            shitsfucked(intent);
+        }
+    }
+
+    public void newStock(Intent intent){
+        Bundle extras = intent.getExtras();
+        String name = extras.getString("company");
+        Double price = Double.valueOf(extras.getString("price"));
+        if(availableStocks == null){
+            Toast.makeText(this, "There was a problem adding " + name + " please try again.", Toast.LENGTH_SHORT);
+        }else{
+            Stock stock = new Stock();
+            String abbr = name;
+            if(name.length() >= 4){
+                abbr = name.substring(0, 4);
+            }
+            ArrayList<Stock> updatedStocks = stock.hashmapToStock(availableStocks);
+            abbr = abbr.toUpperCase();
+            updatedStocks.add(new Stock(name, abbr, price));
+            reference.child("AvailableStocks").setValue(updatedStocks);
+            System.out.println("NEW STOCK ADDED");
+            Toast.makeText(this, "New stock added! " + name + " was added at $" + price + " per share.", Toast.LENGTH_SHORT);
+        }
+    }
+
+    public void priceChange(Intent intent){
+        Bundle extras = intent.getExtras();
+        //TODO: Pull correct extra names out of the bundle
+    }
+
+    public void offMarket(Intent intent){
+        Bundle extras = intent.getExtras();
+        //TODO: Pull correct extra names out of the bundle
+    }
+
+    public void shitsfucked(Intent intent){
+        Bundle extras = intent.getExtras();
+        //TODO: Pull correct extra names out of the bundle
     }
 
     @Override
